e5b21bfb41b389b56d08b5f6fbbf00dbd17e9b44
201524547 <qht6@naver.com>
2021-03-29 15:53:16 +0900

    before Adapt Strategy Pattern

---
+++ StratgyPatternPractice/Robot.java
@@ -0,0 +1,18 @@
+package StratgyPatternPractice;
+
+
+public abstract class Robot {
+	private String name;
+
+	public Robot(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	abstract public void attack();
+
+	abstract public void move();
+}
